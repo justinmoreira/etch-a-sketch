25c75b61947eb80e4cf30a050da887e59c9e8ad7
Justin Moreira <justinmoreira23@gmail.com>
2023-08-26 13:51:48 -0400

    Add grid size button to remove previous grid and create a new one of custom size

--- function.js
+++ function.js
@@ -1,11 +1,22 @@
+function size(){
+    let num = prompt("Please enter the number of cells per side (between 1 and 100)");
+    const grid = document.querySelector('#container');
+    grid.innerHTML = '';
+    setGrid(num);
+}
+
 function setGrid(num){
     const grid = document.querySelector('#container');
     grid.setAttribute("class", "grid");
+    
+    let x = (960/num) - 2;
 
     for(let i = 0; i < num; i++){
         for(let j = 0; j < num; j++){
             const cell = document.createElement('div');
             cell.setAttribute("class", "cellStyle");
+            cell.style.width = x + "px";
+            cell.style.height = x + "px";
             cell.addEventListener('mouseover', function(){
                 this.style.backgroundColor  = "red";
             });
@@ -13,6 +24,8 @@ function setGrid(num){
             grid.append(cell);
         }
     }
+    
+    
 }
 
 setGrid(16);
